baad332d9000621b2c4c5659261cdddc4cc4e96b
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-05-15 01:17:55 +0300

    Jackson örneği eklendi.

---
+++ src/main/java/com/service/JacksonExample.java
@@ -0,0 +1,21 @@
+package com.service;
+
+import com.model.Person;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/jackson")
+public class JacksonExample {
+    @GET
+    @Path("/person")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person getPerson() {
+        Person person = new Person();
+        person.setId(164);
+        person.setName("Muhammed Eren");
+        person.setSurname("Demir");
+        return person;
+
+    }
+}
